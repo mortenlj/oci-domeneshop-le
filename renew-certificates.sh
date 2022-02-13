27a2a6adbcf28f5a9f63eef908506da4caf4411a
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-02-13 21:23:29 +0100

    Save certificate data in tarball to preserve symlinks

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
+set -o errexit # abort on nonzero exitstatus
+set -o nounset # abort on unbound variable
 
 BUCKET_NAME="le-certificates"
+TARBALL_NAME="le-certificates.tar.bz2"
 CERT_PATH="/etc/letsencrypt"
 MAIN_DOMAIN=ibidem.no
 DOMAINS="${MAIN_DOMAIN},*.${MAIN_DOMAIN},*.oci.${MAIN_DOMAIN}"
@@ -13,7 +14,7 @@ OCI_CLI_CONFIG_FILE="${OCI_CLI_CONFIG_FILE:-~/.oci/config}"
 
 tenancy=
 # shellcheck disable=SC1090
-source <(grep tenancy < "${OCI_CLI_CONFIG_FILE}")
+source <(grep tenancy <"${OCI_CLI_CONFIG_FILE}")
 COMPARTMENT_ID="${COMPARTMENT_ID:-${tenancy}}"
 
 export PATH="${PATH}:/opt/oci-sdk/bin:/opt/certbot/bin"
@@ -25,7 +26,19 @@ function log() {
 
 function download_certificates() {
   log "Attempting download of existing certificates"
-  oci os object bulk-download --bucket-name "${BUCKET_NAME}" --download-dir "${CERT_PATH}"
+  target_file=$(mktemp -t certificates-XXXXXXX.tar.bz2)
+  if oci os object get --bucket-name "${BUCKET_NAME}" --name "${TARBALL_NAME}" --file "${target_file}"; then
+    log "Downloaded existing tarball, unpacking"
+    tar xjvf "${target_file}" -C /
+  fi
+}
+
+function upload_certificates() {
+  log "Creating tarball of certificates"
+  target_file=$(mktemp -t certificates-XXXXXXX.tar.bz2)
+  tar cjvf "${target_file}" "${CERT_PATH}"
+  log "Uploading certificates"
+  oci os object put --bucket-name "${BUCKET_NAME}" --name "${TARBALL_NAME}" --file "${target_file}" --force --verify-checksum
 }
 
 function create_certificates() {
@@ -38,12 +51,6 @@ function renew_certificates() {
   log "Renewing certificates"
   certbot renew --config ./certbot.ini --deploy-hook /app/update_certificate_in_LB.sh
 }
-
-function upload_certificates() {
-  log "Uploading certificates"
-  oci os object bulk-upload --no-follow-symlinks --overwrite --bucket-name "${BUCKET_NAME}" --src-dir "${CERT_PATH}"
-}
-
 
 log "Start certificate update job"
 
